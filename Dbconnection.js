870811cbd826a9eabc410bdb8d8dd40dcc96d319
Sharvil <sharvilm777@gmail.com>
2023-09-29 02:25:36 +0530

    Solved DB Connection

--- Dbconnection.js
+++ Dbconnection.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
 // const collectionName = "CryptoLearn-Database";
 // const URI = `mongodb://localhost:27017/${collectionName}?readPreference=primary&appname=MongoDB%20Compass&ssl=false`;
 // const URI = `mongodb+srv://Sharvil:Sharvilm@143@cluster0.6e3jweq.mongodb.net/CryptoLearn?retryWrites=true&w=majority`;
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const dotenv = require("dotenv");
 const cors = require("cors");
+const dotenv = require("dotenv");
+
 dotenv.config();
 const Dbconnection = require("./Dbconnection");
 
