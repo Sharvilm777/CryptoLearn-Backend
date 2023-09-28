5f7f434c377ceb707feb0a79255d90bf369b8e97
Sharvil <sharvilm777@gmail.com>
2023-09-29 02:36:50 +0530

    URI error:Solved

--- Dbconnection.js
+++ Dbconnection.js
@@ -5,7 +5,7 @@ dotenv.config();
 // const collectionName = "CryptoLearn-Database";
 // const URI = `mongodb://localhost:27017/${collectionName}?readPreference=primary&appname=MongoDB%20Compass&ssl=false`;
 // const URI = `mongodb+srv://Sharvil:Sharvilm@143@cluster0.6e3jweq.mongodb.net/CryptoLearn?retryWrites=true&w=majority`;
-console.log(process.env.DB_URI);
+
 const Dbconnection = async () => {
   try {
     await mongoose.connect(process.env.DB_URI);
